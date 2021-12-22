dcb84def2c0bbf3be761f0c13cfea0637861e49f
TheSGJ <60005430+TheSGJ@users.noreply.github.com>
2021-12-22 20:02:40 +0530

    files added!

---
+++ js/music-list.js
@@ -0,0 +1,62 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+    {
+      name: "Hyouhaku + Kokuten - Sasuke Theme",
+      artist: "Naruto Shippuden OST",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Safe & Sound",
+      artist: "Deaf Kev",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "Man of the World",
+      artist: "Yasuharu Takanashi - Naruto Shippuden OST",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "Man of the World - Extended OST",
+      artist: "Yasuharu Takanashi - Naruto Shippuden OST",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: "Everything [NCS Release] ",
+      artist: "Diamond Eyes - NCS Release",
+      img: "music-5",
+      src: "music-5"
+    },
+    {
+      name: "Whole [NCS Release]",
+      artist: "Adam Tell & Rob Gasser Remix - NCS Release",
+      img: "music-6",
+      src: "music-6"
+    },
+    {
+      name: "Belupacito",
+      artist: "Beluga - Belupacito",
+      img: "music-7",
+      src: "music-7"
+    },
+    // like this paste it and remember to give comma after ending of this bracket }
+    // {
+    //   name: "Here is the music name",
+    //   artist: "Here is the artist name",
+    //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+    //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+    // }
+  ];
